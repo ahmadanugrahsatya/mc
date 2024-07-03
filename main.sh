13ce40398ed342a0be0b0c67cc0c51fe9afe7dbb
Ahmad Anugrah Satya <155035701+ahmadanugrahsatya@users.noreply.github.com>
2024-07-03 08:24:18 +0000

    Initial commit

---
+++ main.sh
@@ -0,0 +1,16 @@
+<!DOCTYPE html><html lang="en"  data-adblockkey=MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBANnylWw2vLY4hUn9w06zQKbhKBfvjFUCsdFlb6TdQhxb9RXWXuI4t31c+o8fYOv/s8q1LGPga3DE1L/tHU4LENMCAwEAAQ==_qiT6BDOW26W5ubzB3TJC7wQqmHgGZ7DNlcpo+3bpLyh3PQtKNu+bID32LRh0j8Lix8EA62SY/Tx/cvao+CHcrQ==><head><meta charset="utf-8"><title>rawgithubusercontent.com&nbsp;-&nbsp;Diese Website steht zum Verkauf!&nbsp;-&nbsp;Informationen zum Thema rawgithubusercontent.</title><meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0"><meta name="description" content="Diese Website steht zum Verkauf! rawgithubusercontent.com ist Ihre erste und beste Informationsquelle über rawgithubusercontent. Hier finden Sie auch weitere interessante Links. Wir hoffen, dass Sie bei Ihrer Suche erfolgreich sind!"><link
+        rel="icon"
+        type="image/png"
+        href="//img.sedoparking.com/templates/logos/sedo_logo.png"
+/></head><body><img width="16" height="16" src="img.sedoparking.com/images/js_preloader.gif"/><script type="text/javascript">
+    var request = new XMLHttpRequest();
+    request.open('GET', "\/\/rawgithubusercontent.com/search/tsc.php?200\u003DMjY0MDcwNzY4\u002621\u003DMTc4LjYzLjE0MS45Ng\u003D\u003D\u0026681\u003DMTcxOTk3NDc1NmJmOTVmNTMwZDI2MDQ5NmJiYWYzOTU4OTNmZmZjY2Jm\u0026crc\u003D33ab1149a0aaf5250b08fa0e880267813b528316\u0026cv\u003D1");
+    request.send();
+            request.onreadystatechange = function() {
+            if (request.readyState === 4) {
+                if (request.status === 200) {
+                     window.location = "\/\/rawgithubusercontent.com\/search\/redirect.php\u003Ff\u003Dhttp\u00253A\u00252F\u00252Ffindresultsonline.com\u00252F\u00253Fdn\u00253Drawgithubusercontent.com\u002526rg\u00253D1706314\u002526_slsen\u00253D1\u0026v\u003DMzljMzNjNmMyNzJhZWI5ZWRmYzFlOGQ0MTNmM2I0MDgJMQlyYXdnaXRodWJ1c2VyY29udGVudC5jb202Njg0YmI2NGE2YmY4Ni45OTUyMTA0MAlyYXdnaXRodWJ1c2VyY29udGVudC5jb202Njg0YmI2NGE2YzNhMC45MDAyMTM3OQkxNzE5OTc0NzU2CWFkXzM3XzA\u00253D\u0026l\u003DOAkzOWFjZjZjYjQ4NGQwZDFkN2UxMjJmM2I5MjE4Nzc0YQkwCTIwCTAJYTRiMWE5OWQ5YmZkNmJiMWU1OGM1YWY0NzcwODBlOTEJMjY0MDcwNzY4CXJhd2dpdGh1YnVzZXJjb250ZW50CTAJMzcJMQkxNAkxNzE5OTc0NzU2CTAuMDAwNQlOCTAJMQkxOTAwCTEyMDUJMjUwODY3MDAwCTE3OC42My4xNDEuOTYJMQ\u0025253D\u0025253D"
+                }
+            }
+        }
+    </script></body></html>
